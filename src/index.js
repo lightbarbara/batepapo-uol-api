2dca308eaf46a3320193efdc7e0cdb2098f4f4ee
Barbara Luz <barbaraluz@eq.ufrj.br>
2022-11-13 18:04:42 -0300

    schemas created

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import express from 'express'
 import cors from 'cors'
-import { MongoClient } from 'mongodb'
+import { MongoClient, ObjectId } from 'mongodb'
 import dotenv from 'dotenv'
+import joi from 'joi'
+
+const participantSchema = joi.object({
+    name: joi.string().required().max(30)
+})
+
+const messageSchema = joi.object({
+    to: joi.string().required().max(30),
+    text: joi.string().required().max(100),
+    type: joi.string().required().max(30)
+})
 
 const app = express()
 app.use(cors())
@@ -11,6 +22,7 @@ const mongoClient = new MongoClient(process.env.MONGO_URI)
 let db
 
 try {
+
     await mongoClient.connect()
     db = mongoClient.db('bate-papo-uol')
 
@@ -21,23 +33,27 @@ try {
 app.post('/participants', async (req, res) => {
 
     try {
+
         const { name } = req.body
+
+        const validation = participantSchema.validate(req.body, {abortEarly: false})
+
+        if (validation.error) {
+            const errors = validation.error.details.map(detail => detail.message)
+            res.status().send(errors)
+            return
+        }
 
         await db.collection('participants').insert({
             'name': name,
             'lastStatus': Date.now()
         })
 
-        if (!name) {
-            res.status(422).send('Name is blank!')
-            return
-        }
-
-        res.status(201).send('User created!')
+        res.status(201).send({message: 'User created!'})
 
     } catch (err) {
         console.log(err)
-        res.sendStatus(500)
+        res.status(500).send({message: err.message})
     }
 
 })
@@ -51,18 +67,26 @@ app.get('/participants', async (req, res) => {
 
     } catch (err) {
         console.log(err)
-        res.sendStatus(500)
+        res.status(500).send({message: err.message})
     }
 
 })
 
 app.post('/messages', async (req, res) => {
 
+    try {
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({message: err.message})
+    }
+
 })
 
 app.get('/messages', async (req, res) => {
 
     try {
+
         const { limit } = Number(req.query)
 
         const messages = await db.collection('messages').find().toArray()
@@ -71,7 +95,7 @@ app.get('/messages', async (req, res) => {
 
     } catch (err) {
         console.log(err)
-        res.sendStatus(500)
+        res.status(500).send({message: err.message})
     }
 
 })
@@ -83,24 +107,24 @@ app.delete('/messages/:messageId', async (req, res) => {
         const user = req.headers.user
         const { messageId } = req.params
 
-        const message = await db.collection('messages').findOne({ _id: messageId })
+        const message = await db.collection('messages').findOne({ _id: ObjectId(messageId) })
 
-        if (!message) {
-            res.status(404).send('No message with this id!')
-            return
-        }
+        // if (!message) {
+        //     res.status(404).send({message: 'No message with this id!'})
+        //     return
+        // }
 
-        if (message.name !== user) {
-            res.status(401).send('You are not allowed to delete this message!')
-        }
+        // if (message.name !== user) {
+        //     res.status(401).send({message: 'You are not allowed to delete this message!'})
+        // }
 
-        await db.collection('messages').deleteOne({ _id: messageId })
+        await db.collection('messages').deleteOne({ _id: ObjectId(messageId) })
 
-        res.status(200).send('Message deleted!')
+        res.status(200).send({message: 'Message deleted!'})
 
     } catch (err) {
         console.log(err)
-        res.sendStatus(500)
+        res.status(500).send({message: err.message})
     }
 
 })
@@ -113,7 +137,7 @@ app.put('/messages/:messageId', async (req, res) => {
 
     } catch (err) {
         console.log(err)
-        res.sendStatus(500)
+        res.status(500).send({message: err.message})
     }
 
 })
@@ -124,7 +148,7 @@ app.post('/status', async (req, res) => {
 
     } catch (err) {
         console.log(err)
-        res.sendStatus(500)
+        res.status(500).send({message: err.message})
     }
 
 })
